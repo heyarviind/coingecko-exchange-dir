7d229f076eef33cc6719a3dae8120edde3e9f780
Arvind <heyarviind@gmail.com>
2021-05-25 18:55:43 +0530

    add icons to exchange info

--- src/components/ExchangeRow.js
+++ src/components/ExchangeRow.js
@@ -21,7 +21,37 @@ export default function ExchangeRow({ data }) {
           {name}
         </Name>
         <Info>
-          {country} {trust_score_rank}
+          <span className="inline-flex mr-4">
+            <svg
+              class="w-5 h-5 mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            {country}
+          </span>
+
+          <span className="inline-flex">
+            <svg
+              class="w-5 h-5 mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            {trust_score_rank}
+          </span>
         </Info>
       </div>
       <div>
@@ -56,5 +86,6 @@ export const Name = tw.a`
 
 export const Info = tw.div`
   text-sm
+  mt-1
   text-gray-500
 `;
